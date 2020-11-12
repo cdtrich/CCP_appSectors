5dc705f190ea1f2483aded9aa6c00050b1902e28
cd_trich <31612691+cdtrich@users.noreply.github.com>
2020-11-12 10:59:19 +0100

    size

--- 2_sectors.js
+++ 2_sectors.js
@@ -105,8 +105,10 @@ const createChart = async () => {
 
 	const update = () => {
 		//////////////////////////// sizes ///////////////////////////////////
-		// const size = d3.min([window.innerWidth * 0.9, window.innerHeight * 0.9]);
-		const size = 1100;
+		const el = document.getElementById("col");
+		// const size = 795;
+		// const size = d3.min([window.innerWidth * 0.99, window.innerHeight * 0.99]);
+		const size = el.clientWidth * 0.99;
 
 		let dimensions = {
 			width: size,
